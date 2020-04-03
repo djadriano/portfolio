8c9707f339854de2f9ce70666d2b601eca7a1a31
Adriano Fernandes <afernandes@mirabeau.nl>
2020-04-03 20:15:48 +0200

    style(css): create css variables and typography classes

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,1 +1,4 @@
-<h1>Index Page</h1>
+<p class="fs fs--smallest color color--black">Smallest text</p>
+<p class="fs fs--small color color--gray-light">Small text</p>
+<p class="fs fs--medium color color--gray-dark">Medium text</p>
+<h1 class="fs fs--huge">Huge text</h1>
