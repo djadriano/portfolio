d49aeff8c8d8edb9e1af428acaa6c308573969c1
Adriano Fernandes <afernandes@mirabeau.nl>
2020-05-21 21:42:05 +0200

    refactor(layout-main-element): remove from layout and use in every page, define styles in home page

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,3 +1,11 @@
+<style>
+  main {
+    height: 100%;
+    position: relative;
+    top: calc(-1 * var(--header-height));
+  }
+</style>
+
 <script>
   import { Intro } from '@components/Intro/Index.js';
   import { PageTitle } from '@components/PageTitle/Index.js';
@@ -6,12 +14,16 @@
   import PersonalData from '@data/personal.json';
 </script>
 
-<Intro />
-<section>
-  <PageTitle />
-  <LinksList links="{CasesData.data}" />
-</section>
-<section>
-  <PageTitle title="Personal" />
-  <LinksList links="{PersonalData.data}" />
-</section>
+<main>
+  <Intro />
+
+  <section>
+    <PageTitle />
+    <LinksList links="{CasesData.data}" />
+  </section>
+
+  <section>
+    <PageTitle title="Personal" />
+    <LinksList links="{PersonalData.data}" />
+  </section>
+</main>
