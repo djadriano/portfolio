047814caf8f60ba237d84570a27899934ea2d1fb
Adriano Fernandes <afernandes@mirabeau.nl>
2020-05-21 21:46:58 +0200

    feat(case-page): create layout and template for cases

---
+++ src/pages/case/[slug]/_layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { params, goto } from '@sveltech/routify';
+  import CasesData from '@data/cases.json';
+  export let slug;
+
+  slug = $params.slug;
+  const isPage = CasesData.data.find(item => item.permalink === slug);
+
+  if (!isPage) $goto('/');
+</script>
+
+{#if isPage}
+  <slot />
+{/if}
---
+++ src/pages/case/[slug]/index.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { params } from '@sveltech/routify';
+  import { PageTitle } from '@components/PageTitle/Index.js';
+  import CasesData from '@data/cases.json';
+  export let slug;
+
+  slug = $params.slug;
+  const caseData = CasesData.data.find(item => item.permalink === slug);
+</script>
+
+<main>
+  <PageTitle
+    title="{caseData.name}"
+    subtitle="{caseData.company}"
+    date="{caseData.year}" />
+  <h1>Case name: {slug}</h1>
+</main>
