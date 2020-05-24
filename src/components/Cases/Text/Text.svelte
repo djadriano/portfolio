aa40d71e19bda22f2c00872d26db1cdd50b1c969
Adriano Fernandes <afernandes@mirabeau.nl>
2020-05-24 19:31:57 +0200

    fix(unused-props): remove unused props from components

--- src/components/Cases/Text/Text.svelte
+++ src/components/Cases/Text/Text.svelte
@@ -1,1 +1,10 @@
-<h1>Text component</h1>
+<script>
+  export let content;
+</script>
+
+<p class="fs fs--small color color--gray-dark u-case-block">
+  {#each content as item}
+    {@html item}
+    <br />
+  {/each}
+</p>
--- src/components/Cases/Video/Video.svelte
+++ src/components/Cases/Video/Video.svelte
@@ -1,1 +1,29 @@
-<h1>Video component</h1>
+<style>
+  div {
+    position: relative;
+    padding-bottom: 56.25%;
+    padding-top: 2.5rem;
+    height: 0;
+    border: 1px solid var(--color-gray-light);
+  }
+
+  iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+</style>
+
+<script>
+  export let url;
+</script>
+
+<div class="u-case-block">
+  <iframe
+    src="{url}"
+    frameborder="0"
+    title="video-player"
+    allowfullscreen></iframe>
+</div>
