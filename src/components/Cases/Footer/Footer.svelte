bb1afb266d29ffa2ed8d737be4c6415b350c3063
Adriano Fernandes <afernandes@mirabeau.nl>
2020-05-24 19:33:44 +0200

    feat(cases-page-components): create components for cases page

---
+++ src/components/Cases/Footer/Footer.svelte
@@ -0,0 +1,21 @@
+<style>
+  footer {
+    padding: var(--spaces-s);
+    margin-top: var(--spaces-s);
+  }
+</style>
+
+<script>
+  import { ProjectName } from '@components/ProjectName/Index.js';
+
+  export let name;
+  export let permalink;
+</script>
+
+<footer class="u-project-name">
+  <ProjectName
+    title="{name}"
+    subtitle="Next project"
+    color="black"
+    link="/case/{permalink}" />
+</footer>
---
+++ src/components/Cases/Title/Title.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let text;
+</script>
+
+<h2 class="fs fs--medium u-case-block">
+  <b>{text}</b>
+</h2>
