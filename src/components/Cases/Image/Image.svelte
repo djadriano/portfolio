c098de5fe8ab59ad2a291998a6a06448397cf8e2
Adriano Fernandes <afernandes@mirabeau.nl>
2020-05-25 23:19:25 +0200

    feat(lazy-load-images): use intersection observer to lazy load images

--- src/components/Cases/Image/Image.svelte
+++ src/components/Cases/Image/Image.svelte
@@ -19,18 +19,62 @@
 <script>
   export let common;
   export let sizes;
+
+  let observer;
+
+  function lazyLoad(images) {
+    images.forEach(picture => {
+      if (picture.intersectionRatio > 0) {
+        const sources = [...picture.target.children];
+        const pictureEl = picture.target;
+
+        sources.forEach(source => {
+          if (source.hasAttribute('data-srcset')) {
+            source.setAttribute('srcset', source.getAttribute('data-srcset'));
+          } else {
+            source.setAttribute('src', source.getAttribute('data-src'));
+          }
+
+          source.addEventListener(
+            'load',
+            image => {
+              image.target.closest('picture').classList.remove('lazy-initial');
+            },
+            false
+          );
+        });
+
+        observer.unobserve(pictureEl);
+      }
+    });
+  }
+
+  function setObserver(node) {
+    observer = new IntersectionObserver(lazyLoad, {
+      rootMargin: '100px',
+      threshold: 1.0,
+    });
+
+    observer.observe(node);
+
+    return {
+      destroy() {
+        observer.unobserve(node);
+      },
+    };
+  }
 </script>
 
 <figure class="u-case-block">
-  <picture>
+  <picture use:setObserver>
     {#each sizes as item}
       <source
         media="({item.media})"
-        srcset="/images/cases/{item.src}"
+        data-srcset="/images/cases/{item.src}"
         alt="{item.alt}" />
     {/each}
     {#if common}
-      <img src="/images/cases/{common.src}" alt="{common.alt}" />
+      <img data-src="/images/cases/{common.src}" alt="{common.alt}" />
     {/if}
   </picture>
 </figure>
