0f75bbd5c4a2366d95f694c29c3bc508d9116e84
Adriano Fernandes <afernandes@mirabeau.nl>
2020-06-08 17:13:08 +0200

    feat(cases-list-component): add optional link to list item

--- src/components/Cases/List/List.svelte
+++ src/components/Cases/List/List.svelte
@@ -1,4 +1,16 @@
+<style>
+  li {
+    margin-bottom: var(--spaces-xxs);
+  }
+
+  li:last-of-type {
+    margin-bottom: 0;
+  }
+</style>
+
 <script>
+  import Link from '@components/Link/Link.svelte';
+
   export let title;
   export let items;
 </script>
@@ -10,7 +22,13 @@
   <ul class="u-reset-list">
     {#each items as item}
       <li class="fs fs--small color color--gray-dark">
-        {@html item}
+        {#if item.href}
+          <Link href="{item.href}" fontSize="small" class="" target="_blank">
+            {item.text}
+          </Link>
+        {:else}
+          {@html item.text}
+        {/if}
       </li>
     {/each}
   </ul>
