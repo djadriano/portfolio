98867e3399b1fdc79b8a0d941a22db303025fb74
Adriano Fernandes <afernandes@mirabeau.nl>
2020-08-03 11:44:01 +0200

    refactor(unused-files): remove unused files

--- src/stores/stores.js
+++
@@ -1,7 +0,0 @@
-import { writable } from 'svelte/store';
-
-import CasesData from '@data/cases/list.json';
-import PersonalData from '@data/personal.json';
-
-export const Cases = writable(CasesData.data);
-export const Personal = writable(PersonalData.data);
